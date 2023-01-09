9c3b571227b7a1e093a9535582d0460c27c2eadd
abhi-2107 <abhipan.ap33@gmail.com>
2023-01-09 18:13:30 +0530

    final submission helthcare, hamburger added in mob. version,content updated

---
+++ index.js
@@ -0,0 +1,9 @@
+function toggleClick() {
+    // console.log('working..');
+    let click = document.getElementById('menu-click');
+    if (click.style.display === "block") {
+        click.style.display = "none";
+    } else {
+        click.style.display = "block";
+    }
+}
